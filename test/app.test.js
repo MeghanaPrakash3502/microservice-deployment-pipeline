6944d607d38fb593d7b0182ea3bc320a38ca112d
MeghanaPrakash3502 <meghanamp084@gmail.com>
2026-06-14 16:47:57 +0530

    Test Jenkins auto trigger

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
   it('should return status 200 and a message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe('Microservice is running!');
+    expect(res.body.message).toBe('Microservice is running successfully!');
   });
 });
 
